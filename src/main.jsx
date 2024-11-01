9656252bc80fbe3ffe46919c04e900a9eb44b569
Enes Bayar <103318928+enbayy@users.noreply.github.com>
2024-11-01 22:17:19 +0300

    pages added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import routes from './routes'
 
 createRoot(document.getElementById('root')).render(
-  <div>test</div>
+  <RouterProvider router={routes} />
 )
